950a65ce4bacbe902edee54825a3a629ce9c0ed3
Sadonikov Alexander <alecos100@gmail.com>
2023-07-09 12:10:52 +0300

    2. Try to write integration.

--- slave1/src/main/java/by/sadovnick/slave1/config/IntegrationConfig.java
+++ slave1/src/main/java/by/sadovnick/slave1/config/IntegrationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class IntegrationConfig {
-
     @Bean
     public MessageChannel slave1InputChannel(){
         return new DirectChannel();
--- slave1/src/main/java/by/sadovnick/slave1/processor/Slave1Processor.java
+++ slave1/src/main/java/by/sadovnick/slave1/processor/Slave1Processor.java
@@ -5,7 +5,6 @@ import org.springframework.integration.annotation.ServiceActivator;
 
 @MessageEndpoint
 public class Slave1Processor {
-
     @ServiceActivator(inputChannel = "slave1InputChannel", outputChannel = "slave1OutputChannel")
     public String process(String input) {
         return input.toUpperCase();
